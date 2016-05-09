7c75dc57d7732795b648318de1bfc2addc9efd63
Vince Oveson <vince@aspendigital.com>
2016-05-09 17:17:37 -0600

    Removed bootstrap dependency, improved view tracking

--- js/nimbus/nimbus-load.js
+++ js/nimbus/nimbus-load.js
@@ -13,6 +13,7 @@
    */
 
   var base_url = '';
+  var nimbus_uuid = null;
 
   if (nimbus_base_url) {
     base_url = nimbus_base_url;
@@ -21,9 +22,11 @@
   var config = {
     jquery:           base_url + '/js/nimbus/jquery.min.js',
     jqueryNoConflict: base_url + '/js/nimbus/jquery_noconflict.js',
+    uuid_gen:         base_url + '/js/nimbus/uuid.js',
+    js_cookie:        base_url + '/js/nimbus/js_cookie.js',
     modal: {
-      js:      base_url + '/js/nimbus/bootstrap-modal.js',
-      css:     base_url + '/js/nimbus/bootstrap-modal.css'
+      js:      base_url + '/js/nimbus/vanilla-modal.min.js',
+      css:     base_url + '/js/nimbus/nimbus-modal.css'
     }
   };
 
@@ -41,18 +44,26 @@
     async(config.jqueryNoConflict); // Enable noConflict
   }
 
+  async(config.uuid_gen, function(){
+    async(config.js_cookie, function(){
+      var NimbusCookies = Cookies.noConflict();
+      retrieved_cookie = NimbusCookies.get('nimbus_user');
+
+      if (retrieved_cookie == null)
+      {
+        retrieved_cookie = uuid.v4();
+        NimbusCookies.set('nimbus_user', retrieved_cookie); // TODO: add 'path' and 'expires' options to cookie
+      }
+
+      nimbus_uuid = retrieved_cookie;
+    });
+  });
+
   /* Make sure jQuery is loaded before loading rest of script */
   function checkJquery() {
     if (window.jQuery) {
-      /*
-       * Check Bootstrap modal
-       */
-      checkBootstrapModal();
-
-      /*
-       * Load Nimbus
-       */
-      loadNimbus();
+      
+      loadModalAssets();
 
     } else {
       window.setTimeout(checkJquery, 100);
@@ -72,90 +83,72 @@
      *
      */
     var modalHtml = [];
-    modalHtml.push('<div class="modal modal-lg fade" id="nimbus-modal-target">');
-      modalHtml.push('<div class="modal-dialog">');
-        modalHtml.push('<div class="modal-content">');
-          modalHtml.push('<div class="modal-header">');
-            modalHtml.push('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
-          modalHtml.push('</div>');
-          modalHtml.push('<div class="modal-body">');
-            modalHtml.push('<iframe src="" scrolling="no" frameborder="0" width="100%" height="100%"></iframe>');
-          modalHtml.push('</div>');
-        modalHtml.push('</div>');
+
+    /* Write modal html */
+    modalHtml.push('<div id="nimbus-modal-target" class="nimbus-modal">');
+      modalHtml.push('<div class="nimbus-modal-inner modal-dialog">');
+        modalHtml.push('<div class="nimbus-modal-content"></div>');
       modalHtml.push('</div>');
     modalHtml.push('</div>');
+    modalHtml.push('<div id="nimbus-modal-1" style="display:none">');
+      modalHtml.push('<a rel="modal:close" class="close">&times;</a>');
+      modalHtml.push('<iframe id="nimbus-iframe" scrolling="no" frameborder="0" width="100%" height="100%"></iframe>');
+    modalHtml.push('</div>');
+    modalHtml.push('<div class="modal-backdrop fade" style="z-index: -1"></div>');
 
     modalHtml = modalHtml.join('');
 
-    jQuery(document).ready(function($) {
-      /*
-       * Append modal HTML inside body tag on ducument.ready
-       *
-       */
-      $('body').append(modalHtml);
+    /*
+     * Append modal HTML inside body tag
+     *
+     */
+    jQuery('body').append(modalHtml);
 
-
-      /*
-       * Apply Bootstrap/Prototype patch
-       *
-       */
-      bootstrapModalPatch();
-
-
-      /*
-       * Modal open and close
-       *
-       */
-
-      /*
-       * Modal open
-       * Grab trigger href and use it as iframe src
-       */
-      $('.nimbus-modal-trigger').on('click', function(e) {
-        e.preventDefault();
-        var $iframe = $(this).attr('href');
-
-        $('#nimbus-modal-target').modal('show');
-        $('#nimbus-modal-target').find('iframe').attr('src', $iframe);
-      });
-
-      /*
-       * Modal close
-       * Reset the iframe on close
-       */
-      $('#nimbus-modal-target').on('hidden.bs.modal', function(e) {
-        $('#nimbus-modal-target').find('iframe').attr('src', '');
-      });
+    // Initialize modal
+    // Instantiate Vanilla Modal
+    const nimbus_modal = new vanillaModal.VanillaModal({
+      modal: '.nimbus-modal',
+      modalInner: '.nimbus-modal-inner',
+      modalContent: '.nimbus-modal-content',
+      onBeforeOpen : function(e) {
+        jQuery('#nimbus-modal-1').css({ display: 'block' });
+        jQuery('#nimbus-iframe').attr('src', jQuery('.nimbus-modal-trigger').data('url'));
+        jQuery('#nimbus-modal-target').addClass('modal-open');
+        jQuery('.modal-backdrop').addClass('in').css('z-index', 1040);
+      },
+      onClose : function(e) {
+        jQuery('#nimbus-modal-1').css({ display: 'none' });
+        jQuery('#nimbus-iframe').attr('src', '');
+        jQuery('#nimbus-modal-target').removeClass('modal-open');
+        jQuery('.modal-backdrop').removeClass('in').css('z-index', -1);
+      }
     });
   }
 
 
   /**************************
-   * Check Bootstrap modal & load modal styles
+   * Load modal script and stylesheet
    *
    */
-  function checkBootstrapModal() {
-    /* Add modal script */
-    if (typeof(jQuery.fn.modal) == 'undefined') {
-      async(config.modal.js);
-    }
+  function loadModalAssets()
+  {
+    /* Load modal styles */
+    var styles = document.createElement('link');
 
-    /* Add Bootstrap modal styles */
-    var modalStyles = document.createElement('link');
+    styles.rel  = 'stylesheet';
+    styles.type = 'text/css';
+    styles.href = config.modal.css;
 
-    modalStyles.rel  = 'stylesheet';
-    modalStyles.type = 'text/css';
-    modalStyles.href = config.modal.css;
+    if (typeof styles !== 'undefined')
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('link')[0]).appendChild(styles);
 
-    if (typeof modalStyles !== 'undefined')
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('link')[0]).appendChild(modalStyles);
+    /*
+     * Load Vanilla Modal script
+     *
+     * https://github.com/thephuse/vanilla-modal
+     */
+    async(config.modal.js, loadNimbus);
   }
-
-
-  /**************************
-   * Helpers functions
-   *
-   */
 
   /*
    * Provide callback to injected scripts
@@ -167,29 +160,6 @@
     o.src = u;
     if (c) { o.addEventListener('load', function (e) { c(null, e); }, false); }
     s.parentNode.insertBefore(o, s);
-  }
-
-  /*
-   * A patch for Bootstrap to work with Prototype.js
-   * http://jsfiddle.net/dgervalle/hhBc6/
-   */
-  function bootstrapModalPatch() {
-    if (Prototype.BrowserFeatures.ElementExtensions) {
-      var disablePrototypeJS = function (method, pluginsToDisable) {
-        var handler = function (event) {
-          event.target[method] = undefined;
-          setTimeout(function () {
-            delete event.target[method];
-          }, 0);
-        };
-        pluginsToDisable.each(function (plugin) {
-          jQuery(window).on(method + '.bs.' + plugin, handler);
-        });
-      },
-      pluginsToDisable = ['modal'];
-      disablePrototypeJS('show', pluginsToDisable);
-      disablePrototypeJS('hide', pluginsToDisable);
-    }
   }
 
 })();

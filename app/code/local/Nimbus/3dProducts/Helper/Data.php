ded1079dd3182bd79ea392d6de8959d4ce20cf3b
Peter Halverson <pete@aspendigital.com>
2017-05-25 15:27:01 -0600

    Update API base URL

--- app/code/local/Nimbus/3dProducts/Helper/Data.php
+++ app/code/local/Nimbus/3dProducts/Helper/Data.php
@@ -15,7 +15,7 @@ class Nimbus_3dproducts_Helper_Data extends Mage_Core_Helper_Data {
 
         // set default API URL when empty
         if (empty($config['api_base_url']))
-        	$this->api_base_url = 'https://api.nimbusreality.com';
+        	$this->api_base_url = 'https://api.interact3d.com';
         else
         	$this->api_base_url = $config['api_base_url'];
 
